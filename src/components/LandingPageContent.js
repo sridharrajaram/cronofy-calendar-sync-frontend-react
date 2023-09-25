97ad7eba0b179701cd48efac22469f28a7d6c0c6
Sridhar Rajaram <srilumia@hotmail.com>
2023-09-25 12:12:06 +0530

    landingpage content change

--- src/components/LandingPageContent.js
+++ src/components/LandingPageContent.js
@@ -6,7 +6,7 @@ import laptops from '../assets/images/laptops.jpg';
 function LandingPageContent() {
     return (
         <div className="container mt-2">
-            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+            {/* <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
                 <div class="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -31,11 +31,20 @@ function LandingPageContent() {
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Next</span>
                 </button>
-            </div>
+            </div> */}
+                <p>
+                    Amet dolore id aute minim nisi excepteur consectetur elit esse in mollit elit minim quis. Exercitation et aute ipsum ea exercitation sit Lorem sint culpa tempor Lorem ipsum. Voluptate fugiat nulla minim sit sit Lorem Lorem ullamco aute nulla. Minim laboris cillum velit laboris officia quis Lorem elit adipisicing duis velit anim. Eu dolore magna velit officia consectetur ipsum consectetur in deserunt aute. Excepteur id consequat ea Lorem nulla voluptate ea consequat pariatur sint qui. Cillum sit occaecat nostrud reprehenderit consequat tempor eu laborum nulla sint duis ad.
+                </p>
+                <p>
+                    Voluptate quis mollit velit irure esse nulla qui Lorem. Consectetur magna reprehenderit nisi in veniam deserunt anim mollit irure quis deserunt excepteur adipisicing. Quis occaecat ullamco adipisicing laborum reprehenderit Lorem.
+                </p>
+                <p>
+                    Ut commodo dolore ipsum anim pariatur nulla magna. Aliqua sunt do sint enim magna consectetur id sunt. Ea cillum excepteur incididunt et voluptate. Officia aliqua aliquip commodo ad exercitation.
+                </p>
+                <p>
+                    Occaecat officia qui pariatur consectetur laboris. Aliquip mollit est ut ut adipisicing ea nisi esse ea deserunt. Aute occaecat quis ut esse id ipsum. Non ipsum anim qui ipsum occaecat pariatur deserunt laboris esse proident commodo anim esse laborum. Tempor excepteur aliqua ipsum enim do id laborum est ullamco officia incididunt ut do commodo. Sunt eu nostrud excepteur sint quis dolor amet mollit tempor. In ipsum magna non non minim.
+                </p>
         </div>
-        // <div className="container">
-        //     <img src={cycle} alt="food1"/>
-        // </div>
     )
 }
 
