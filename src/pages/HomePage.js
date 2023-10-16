32a8ef42e6dcad75b45c80b940a839431a418fe6
Sridhar Rajaram <srilumia@hotmail.com>
2023-10-17 01:00:14 +0530

    added cronofy oauth flow

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,15 +2,86 @@ import React, { useEffect, useState } from 'react'
 import PrivateLayout from '../components/Layout/PrivateLayout'
 import LandingPage from './LandingPage';
 import axios from 'axios';
-import { useLocation } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const backendUrl = "http://localhost:5001"
 
+const cronofyClientId = 'JjMr0DO7uRL31jVr9M0FS6szZeSXTnbQ'; // Replace with your Cronofy Client ID
+const cronofyRedirectUri = 'http://localhost:3000/'; // Replace with your OAuth redirect URI
+const cronofyOauthUrl = 'https://app.cronofy.com/oauth/authorize';
+const cronofyScope = 'read_events create_event delete_event'
+
 function HomePage() {
-  const location = useLocation(); // If using React Router
+  axios.defaults.withCredentials = true;
   const [auth, setAuth] = useState(false);
   const [name, setName] = useState('')
-  axios.defaults.withCredentials = true;
+  const [emailList, setEmailList] = useState([]);
+  const [emailInput, setEmailInput] = useState('');
+
+
+  const handleInputChange = (event) => {
+    setEmailInput(event.target.value);
+  };
+
+
+  const initiateCronofyOAuth = async () => {
+    try {
+      // Define the OAuth authorization URL
+      const authorizationUrl = `${cronofyOauthUrl}?client_id=${cronofyClientId}&redirect_uri=${cronofyRedirectUri}&scope=${cronofyScope}&response_type=code`;
+
+      // You may include additional parameters such as scope, state, and others as needed
+      // For example: &scope=read_events&state=your_state_parameter
+
+      // Return the authorization URL to use in the redirection
+      return {
+        authorizationUrl,
+      };
+    } catch (error) {
+      throw new Error('Failed to initiate Cronofy OAuth');
+    }
+  };
+
+  const handleAddEmail = async (e) => {
+    e.preventDefault()
+    // Regular expression for email validation
+    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+
+    if (emailInput.trim() !== '' && emailRegex.test(emailInput)) {
+      // Check if the email already exists in the list
+      if (!emailList.includes(emailInput)) {
+        setEmailList([...emailList, emailInput]);
+        setEmailInput('');
+
+        // Redirect to Cronofy OAuth flow
+        try {
+          const response = await initiateCronofyOAuth();
+          // You should handle the OAuth redirection and callback here
+          // The response should contain the authorization URL from Cronofy
+          // Redirect the user to the Cronofy authorization URL
+          window.location.href = response.authorizationUrl;
+        } catch (error) {
+          showErrorToast('Failed to initiate Cronofy OAuth.');
+        }
+
+
+      } else {
+        showErrorToast('This email address is already added in your list');
+      }
+    } else {
+      showErrorToast('Please enter a valid email address.');
+    }
+  };
+
+  const handleRemoveEmail = (indexToRemove) => {
+    setEmailList(emailList.filter((_, index) => index !== indexToRemove));
+  };
+
+  const showErrorToast = (message) => {
+    toast.error(message, {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  };
 
   const getUserVerified = async () => {
     try {
@@ -32,53 +103,42 @@ function HomePage() {
     }
   }
 
-  const getCronofyOuth = async () => {
-    
-    const params = new URLSearchParams(location.search); // If using React Router, use location.search
-    console.log('params',params)
-    if (params.has('code')) {
-      const authorizationCode = params.get('code');
-
-      // Send the authorization code to your Node.js backend
-      await axios.post('/cronofy-callback', { code: authorizationCode })
-        .then(response => {
-          console.log('OAuth callback handled:', response.data);
-          // You can redirect the user to a success page or perform other actions here
-        })
-        .catch(error => {
-          console.error('Error handling OAuth callback:', error);
-          // You can redirect the user to an error page or show an error message here
-        });
-    }
-  }
-
   useEffect(() => {
     getUserVerified()
-    getCronofyOuth()
   }, [])
-
-  // Function to initiate the Cronofy OAuth flow
-  const initiateCronofyOAuth = async () => {
-    // Make a GET request to the /cronofy-auth route
-    await axios.get('/cronofy-auth')
-      .then(response => {
-        // Redirect the user to the Cronofy authorization page
-        window.location = response.data;
-      })
-      .catch(error => {
-        console.error('Error initiating Cronofy OAuth:', error);
-      });
-  };
-
 
   return (
     auth ?
       <PrivateLayout>
         <div className="container mt-4">
-          <h3>Hi {name}, <br />Welcome to Cronofy Calendar Sync</h3>
-          <hr />
-          <h1>Cronofy Calendar Integration</h1>
-          <button onClick={initiateCronofyOAuth}>Connect to Cronofy</button>
+          <h3>Hi {name}, <br />Welcome to Personal Calendar Sync - Cronofy API</h3>
+          <div className="row mt-4">
+            <div className="card mb-3" style={{ maxWidth: "30rem" }}>
+              <div className="card-body">
+                <form>
+                  <label for="emailInput"> <strong>Add Email Address:</strong><br />
+                    <input type="email" id="emailInput" value={emailInput} placeholder='janedoe@gmail.com' onChange={handleInputChange} />
+                  </label> &nbsp;
+                  <button onClick={handleAddEmail}><i className="fas fa-plus"></i></button>
+                </form>
+              </div>
+              <div className="card-footer bg-transparent border-success">
+                <h5>
+                  List of Email Address Synced:
+                </h5>
+                <ol className="list-group">
+                  {emailList.map((email, index) => (
+                    <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+                      {email}
+                      <span style={{ color: "red" }} onClick={() => handleRemoveEmail(index)}>
+                        X
+                      </span>
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
+          </div>
         </div>
       </PrivateLayout>
       :
